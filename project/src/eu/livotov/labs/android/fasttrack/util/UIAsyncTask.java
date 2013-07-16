4290d766a88f8676dd968ec7af9e2fc7d9315e2b
Dmitri Livotov <dmitri@livotov.eu>
2013-07-16 18:22:06 +0300

    - added feature to delay progress dialog in UIAsyncTask for specified number of ms

--- project/src/eu/livotov/labs/android/fasttrack/util/UIAsyncTask.java
+++ project/src/eu/livotov/labs/android/fasttrack/util/UIAsyncTask.java
@@ -5,6 +5,9 @@ import android.util.Log;
 import eu.livotov.labs.android.fasttrack.base.BaseActivity;
 import eu.livotov.labs.android.robotools.async.RTAsyncTask;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * (c) Livotov Labs Ltd. 2012
  * Date: 15/07/2013
@@ -18,6 +21,8 @@ public abstract class UIAsyncTask<Param, Progress, Result> extends RTAsyncTask<P
     protected boolean useActionBarProgress = true;
     protected boolean showProgress = true;
     protected boolean cancellable;
+    private Timer busyTimer;
+    private int delayedProgress = 0;
 
     public UIAsyncTask(BaseActivity activity)
     {
@@ -55,6 +60,12 @@ public abstract class UIAsyncTask<Param, Progress, Result> extends RTAsyncTask<P
         return this;
     }
 
+    public UIAsyncTask delayProgressMessage(int delayMs)
+    {
+        this.delayedProgress = delayMs;
+        return this;
+    }
+
     public Result performExecutionThread(final Param... parameters) throws Exception
     {
         return onUITaskBody(parameters);
@@ -62,11 +73,35 @@ public abstract class UIAsyncTask<Param, Progress, Result> extends RTAsyncTask<P
 
     public void onExecutionStarted()
     {
-        showTaskProgressDialog();
+        if (delayedProgress > 0)
+        {
+            busyTimer = new Timer();
+            busyTimer.schedule(new TimerTask()
+            {
+                public void run()
+                {
+                    if (isActivityAlive())
+                    {
+                        activity.runOnUiThread(new Runnable()
+                        {
+                            public void run()
+                            {
+                                UIAsyncTask.this.showTaskProgressDialog();
+                            }
+                        });
+                    }
+                }
+            }, delayedProgress);
+        } else
+        {
+            showTaskProgressDialog();
+        }
     }
 
     public void onExecutionFinished(final Result result)
     {
+        cancelBusyTimer();
+
         if (isActivityAlive())
         {
             hideTaskPrograssDialog();
@@ -79,6 +114,7 @@ public abstract class UIAsyncTask<Param, Progress, Result> extends RTAsyncTask<P
 
     public void onExecutionFailed(final Throwable error)
     {
+        cancelBusyTimer();
         Log.e(activity.getClass().getSimpleName(), error.getMessage(), error);
 
         if (isActivityAlive())
@@ -94,17 +130,14 @@ public abstract class UIAsyncTask<Param, Progress, Result> extends RTAsyncTask<P
 
     public void onExecutionAborted()
     {
+        cancelBusyTimer();
         userCancelled = true;
+
         if (isActivityAlive())
         {
             hideTaskPrograssDialog();
             onUITaskCancelled();
         }
-    }
-
-    private boolean isActivityAlive()
-    {
-        return !activity.isFinishing() && !activity.isFinishing();
     }
 
     public void onCancel(final DialogInterface dialogInterface)
@@ -127,6 +160,25 @@ public abstract class UIAsyncTask<Param, Progress, Result> extends RTAsyncTask<P
         if (showProgress)
         {
             activity.hideProgressDialog();
+        }
+    }
+
+    private boolean isActivityAlive()
+    {
+        return !activity.isFinishing() && !activity.isFinishing();
+    }
+
+    private void cancelBusyTimer()
+    {
+        if (busyTimer != null)
+        {
+            try
+            {
+                busyTimer.cancel();
+                busyTimer = null;
+            } catch (Throwable ignored)
+            {
+            }
         }
     }
 
